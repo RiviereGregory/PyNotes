d4cb08d4d9891bca5ed795bc35f86afd9fd050b7
RiviereGregory <gregory.riviere@laposte.net>
2020-12-09 19:55:03 +0100

    Ajout méthode path dans la class Note

--- src/main/python/package/api/note.py
+++ src/main/python/package/api/note.py
@@ -1,4 +1,7 @@
+import os
 from uuid import uuid4
+
+from package.api.constants import NOTES_DIR
 
 
 class Note:
@@ -13,7 +16,11 @@ class Note:
     def __str__(self):
         return self.title
 
+    @property
+    def path(self):
+        return os.path.join(NOTES_DIR, self.uuid + ".json")
+
 
 if __name__ == '__main__':
     n = Note(title="Ceci est une note", content="ceci est un contenu")
-    print(repr(n))
+    print(n.path)
