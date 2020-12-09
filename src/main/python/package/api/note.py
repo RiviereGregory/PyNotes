98b4b3ab8543012006e9f34f00c75431cf802965
RiviereGregory <gregory.riviere@laposte.net>
2020-12-09 19:36:35 +0100

    Ajout class Note avec main pour test

---
+++ src/main/python/package/api/note.py
@@ -0,0 +1,12 @@
+from uuid import uuid4
+
+class Note:
+    def __init__(self, title="", content="", uuid=None):
+        self.uuid = str(uuid4())
+        self.title = title
+        self.content = content
+
+
+if __name__ == '__main__':
+    n = Note(title="Ceci est une note", content="ceci est un contenu")
+    print(n.uuid)
