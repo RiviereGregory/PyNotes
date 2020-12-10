d59ca87ec0fd7af079cc19f795ad9d9cc680445e
RiviereGregory <gregory.riviere@laposte.net>
2020-12-10 20:39:20 +0100

    Ajout création des widgets et layout

--- src/main/python/package/main_window.py
+++ src/main/python/package/main_window.py
@@ -1,30 +1,34 @@
 from PySide2 import QtWidgets
 
-class MainWindow(QtWidgets.QMainWindow):
+
+class MainWindow(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("PyNotes")
         self.setup_ui()
 
-
     def setup_ui(self):
         self.create_widgets()
-        self.modify_widgets()
         self.create_layouts()
+        self.modify_widgets()
         self.add_widgets_to_layouts()
         self.setup_connections()
 
     def create_widgets(self):
-        pass
+        self.btn_createNote = QtWidgets.QPushButton("Créer une note")
+        self.lw_notes = QtWidgets.QListWidget()
+        self.te_contenu = QtWidgets.QTextEdit()
 
     def modify_widgets(self):
         pass
 
     def create_layouts(self):
-        pass
+        self.main_layout = QtWidgets.QGridLayout(self)
 
     def add_widgets_to_layouts(self):
-        pass
+        self.main_layout.addWidget(self.btn_createNote, 0, 0, 1, 1)
+        self.main_layout.addWidget(self.lw_notes, 1, 0, 1, 1)
+        self.main_layout.addWidget(self.te_contenu, 0, 1, 2, 1)
 
     def setup_connections(self):
         pass
